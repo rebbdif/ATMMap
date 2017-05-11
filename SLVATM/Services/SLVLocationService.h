79072b7d288f4b168da8458a00e87bf55459e2ca
Leonid Serebryanyy <leosilver@yandex.ru>
2017-05-11 13:02:08 +0300

    added slvlocationservice class

---
+++ SLVATM/Services/SLVLocationService.h
@@ -0,0 +1,13 @@
+//
+//  SLVLocationService.h
+//  SLVATM
+//
+//  Created by 1 on 11.05.17.
+//  Copyright © 2017 serebryanyy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SLVLocationService : NSObject
+
+@end
---
+++ SLVATM/Services/SLVLocationService.m
@@ -0,0 +1,13 @@
+//
+//  SLVLocationService.m
+//  SLVATM
+//
+//  Created by 1 on 11.05.17.
+//  Copyright © 2017 serebryanyy. All rights reserved.
+//
+
+#import "SLVLocationService.h"
+
+@implementation SLVLocationService
+
+@end
